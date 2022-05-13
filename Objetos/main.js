9a029139ec4b5bc1adc80a0a3131a07f329edeec
JoaquinMoscon26 <joakomoscon@gmail.com>
2022-05-13 03:35:04 -0300

    Objetos y Arreglos

---
+++ Objetos/main.js
@@ -0,0 +1,25 @@
+let alumno = {
+    nombre : 'Joaquin',
+    apellido : 'Moscon',
+    dni : 45389579,
+    correos : {
+        outlook : 'Joaquin26@outlook.com',
+        gmail : 'joakomoscon@gmail.com',
+        yahoo : 'JoaquinM26@yahoo.com'
+     }
+
+}
+// forma 1
+// const yahoo = alumno.correos.yahoo
+// const gmail = alumno.correos.gmail
+// const outlook = alumno.correos.outlook
+
+// forma 2
+const { yahoo, gmail, outlook } = alumno.correos
+
+console.log(`
+    correos: 
+    Gmail: ${gmail}
+    Yahoo: ${yahoo}
+    Outlook: ${outlook}
+`)
