052c612eff38b8586e8268cf93c566e38723d38e
JoaquinMoscon26 <joakomoscon@gmail.com>
2022-06-10 15:05:41 -0300

    REPASO TP2 HAY QUE SEGUIRLO

---
+++ REPASO-TP2-PARTE_1/main.js
@@ -0,0 +1,18 @@
+let consolas = ['Playstation','Xbox','Nintendo Game Cube','Sega DreamCast','Game Boy Advanced']
+
+//agrego la posición en la que me quiero parar para eliminar hacia adelante
+const posicion = 2;
+//elijo la cantidad de elementos que quiero eliminar
+const numeroElementos = 2;
+
+consolas.splice(posicion,numeroElementos)
+
+//agrego un elemento al final de un arreglo (o array).
+consolas.push('Nintendo 64')
+
+consolas.unshift('Nintendo Wii')
+
+const mostrar = () => 
+{
+    const p_longitud= document.getElementById
+}
