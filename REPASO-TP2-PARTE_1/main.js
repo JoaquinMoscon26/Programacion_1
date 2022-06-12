a4cd3d303e0ba2df67b02cd7a6087e2a4ea6d820
JoaquinMoscon26 <joakomoscon@gmail.com>
2022-06-12 18:41:27 -0300

    cambios repaso TP2

--- REPASO-TP2-PARTE_1/main.js
+++ REPASO-TP2-PARTE_1/main.js
@@ -10,6 +10,7 @@ consolas.splice(posicion,numeroElementos)
 //agrego un elemento al final de un arreglo (o array).
 consolas.push('Nintendo 64')
 
+
 consolas.unshift('Nintendo Wii')
 
 
@@ -22,13 +23,35 @@ const mostrar = () =>
     const ultimo_elemento = consolas [longitud-1]
     //obtengo la posición (índice/index) de un elemento.
     const indice_Xbox = consolas.indexOf("Xbox")
+
+    //accedo al elemento "Xbox", previamente obteniendo su posición.
+    const Xbox_elemento = consolas[indice_Xbox]
     
-    document.getElementById("p_text").textContent= 
-    `
-    la longitud del arreglo es: ${longitud}
-    El último elemento del arreglo es: ${ultimo_elemento}
-    Mostrando en pantalla el elemento: ${indice_Xbox}
-    `
+    document.getElementById("p_text").textContent= 'la longitud del arreglo es: ' +longitud+' El último elemento del arreglo es: '+ultimo_elemento+'Mostrando en pantalla el elemento: '+ Xbox_elemento
+
 }
 
-document.getElementById("btn_arreglo1").addEventListener('click',mostrar())
+document.getElementById("btn_arreglo1").addEventListener('click',mostrar)
+
+function generar()
+{
+    let filas= []
+    consolas.forEach((element,index) => {
+        
+        
+        let fila=
+        
+        `<tr>
+            <td>${index}</td>
+            <td>${element}</td>
+            
+        </tr>`
+        consolas.push(fila)
+        
+    });
+    
+    
+    document.getElementById("tbl_body").innerHTML = filas.join('')
+}
+
+generar()
